4f3819b2625cb6b72b1314289ef7457ef381bb12
gopivaka108 <gopivaka108@gmail.com>
2026-03-24 23:52:05 +0530

    feat: add user input and dynamic array allocation

--- C++_Basics/Reverse_Array.cpp
+++ C++_Basics/Reverse_Array.cpp
@@ -1,4 +1,12 @@
 #include<iostream>
 int main () {
+    int size;
+    std::cout<<"Enter size of Array: ";
+    std::cin>>size;
+    std::cout<<"Enter "<<size<<" Elements: ";
+    int* arr = new int[size];
+    for(int i=0; i<size; i++) {
+        std::cin>>arr[i];
+    }
     return 0;
 }
