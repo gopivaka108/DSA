4f8bdcd20bc1ad4160143a44da1ffc021bf1d760
gopivaka108 <gopivaka108@gmail.com>
2026-03-25 22:48:20 +0530

    feat: implement sum and absolute difference logic

--- C++_Basics/Sum_and_Diff.cpp
+++ C++_Basics/Sum_and_Diff.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 
 void Sum_and_Diff(int &a, int &b) {
-    
+    int temp = a;
+    a = a + b;
+    b = abs(temp - b);
 }
 
 int main () {
