5f78c74060cb146f1c3be87a2883dc4ebe98687a
gopivaka108 <gopivaka108@gmail.com>
2026-03-30 23:50:27 +0530

    feat: define function signature and initialize two pointers

--- C++_Basics/Palindrome_Check.cpp
+++ C++_Basics/Palindrome_Check.cpp
@@ -1,6 +1,11 @@
 #include<iostream>
 #include<string>
 
+bool Palindrome_Check(const std::string& str) {
+    int front = 0, rear = str.size() - 1;
+    return true;
+}
+
 int main () {
     return 0;
 }
