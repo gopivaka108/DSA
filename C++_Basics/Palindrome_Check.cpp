2e8fc061fdccf4a63bd980bb4c692f274babe3bc
gopivaka108 <gopivaka108@gmail.com>
2026-03-30 23:50:44 +0530

    feat: implement two-pointer comparison logic for palindromes

--- C++_Basics/Palindrome_Check.cpp
+++ C++_Basics/Palindrome_Check.cpp
@@ -3,6 +3,13 @@
 
 bool Palindrome_Check(const std::string& str) {
     int front = 0, rear = str.size() - 1;
+    while(front < rear) {
+        if(str[front] != str[rear]) {
+            return false;
+        }
+        front++;
+        rear--;
+    }
     return true;
 }
 
